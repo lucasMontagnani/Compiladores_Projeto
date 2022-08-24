c4d2ccdc915f6294cf34f8d897c2edb8a45a1199
Renan Lima <renan.flimabr@gmail.com>
2022-08-24 18:49:16 -0300

    fix readInt() in .java

--- MainClass.java
+++ MainClass.java
@@ -2,15 +2,25 @@ import java.util.Scanner;
 public class MainClass{ 
   public static void main(String args[]){
  		Scanner _key = new Scanner(System.in);
-		double  a;
-		double  b;
-		a= _key.nextDouble();
-		b= _key.nextDouble();
-		a = 1+b;
-		if (a>b) {
-			System.out.println(a);
+		int  a;
+		String  tInt2;
+		int  b;
+		int  c;
+		String  welcome;
+		String  tInt;
+		welcome = "IsiLanguagem em Ação!";
+		tInt = "Digite um inteiro: ";
+		a= _key.nextInt();
+		tInt2 = "Digite outro inteiro: ";
+		b= _key.nextInt();
+		if (a==b) {
+			System.out.println("Os dois inteiros são iguais!");
+		}
+		c = 2*a;
+		if (b==c) {
+			System.out.println("O segundo inteiro é o dobro do primeiro!");
 		} else {
-			System.out.println(b);
+			System.out.println("O segundo inteiro não é o dobro do primeiro!");
 		}
   }
 }
--- src/br/com/professorisidro/isilanguage/ast/CommandLeitura.java
+++ src/br/com/professorisidro/isilanguage/ast/CommandLeitura.java
@@ -14,7 +14,7 @@ public class CommandLeitura extends AbstractCommand {
 	@Override
 	public String generateJavaCode() {
 		// TODO Auto-generated method stub
-		return "		" + id +"= _key." + (var.getType()==IsiVariable.NUMBER? "nextDouble();": "nextLine();");
+		return "		" + id +"= _key." + (var.getType()==IsiVariable.NUMBER? "nextDouble();": var.getType()==IsiVariable.INT? "nextInt();": var.getType()==IsiVariable.BOOLEAN?"nextBoolean()":"nextLine();");
 	}
 	@Override
 	public String toString() {
